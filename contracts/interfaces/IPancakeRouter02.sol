a2583058daa61a8066c1b5592fded5cae652ba82
Your Name <you@example.com>
2024-09-28 17:51:08 +0100

    more interfaces added toexpand arbitrage contract scope

--- contracts/interfaces/IPancakeRouter02.sol
+++ contracts/interfaces/IPancakeRouter02.sol
@@ -1,16 +1,53 @@
-// ==== This interface defines the functions of PancakeRouter that your contract interacts with. ===
+// ==== This interface defines the functions of PancakeRouter that the contract interacts with. ===
 
 // SPDX-License-Identifier: MIT
-   pragma solidity ^0.8.0;
+pragma solidity ^0.8.0;
 
-   interface IPancakeRouter02 {
-       function swapExactTokensForTokens(
-           uint amountIn,
-           uint amountOutMin,
-           address[] calldata path,
-           address to,
-           uint deadline
-       ) external returns (uint[] memory amounts);
+import "./IPancakeRouter01.sol";
 
-      // function getAmountsOut(uint amountIn, address[] memory path) external view returns (uint[] memory amounts);
-   }
+interface IPancakeRouter02 is IPancakeRouter01 {
+    function removeLiquidityETHSupportingFeeOnTransferTokens(
+        address token,
+        uint256 liquidity,
+        uint256 amountTokenMin,
+        uint256 amountETHMin,
+        address to,
+        uint256 deadline
+    ) external returns (uint256 amountETH);
+
+    function removeLiquidityETHWithPermitSupportingFeeOnTransferTokens(
+        address token,
+        uint256 liquidity,
+        uint256 amountTokenMin,
+        uint256 amountETHMin,
+        address to,
+        uint256 deadline,
+        bool approveMax,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external returns (uint256 amountETH);
+
+    function swapExactTokensForTokensSupportingFeeOnTransferTokens(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external;
+
+    function swapExactETHForTokensSupportingFeeOnTransferTokens(
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external payable;
+
+    function swapExactTokensForETHSupportingFeeOnTransferTokens(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external;
+}
